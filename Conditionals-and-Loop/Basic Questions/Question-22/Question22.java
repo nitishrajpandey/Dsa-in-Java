35ec7e6a0d5f875079e96f3c8c098ecd422f311d
Nitish Raj <nitishrajpandey0707@gmail.com>
2024-03-07 09:30:45 +0530

    add conditional basic question

--- Conditionals-and-Loop/Basic Questions/Question-22/Question22.java
+++ Conditionals-and-Loop/Basic Questions/Question-22/Question22.java
@@ -2,17 +2,21 @@ import java.util.Scanner;
 
 public class Question22 {
     public static void main(String[] args) {
-        System.out.println("*** Subtract the product and sum of Digits of an integer *****");
+        System.out.println(" **** Subtract the Product and Sum of Digits of an Integer *****");
         Scanner inputData = new Scanner(System.in);
-        System.out.println("Enter the first number : ");
-        float number1 = inputData.nextFloat();
-        System.out.println("Enter the second number : ");
-        float number2 = inputData.nextFloat();
-        float product = number1*number2;
-        System.out.println("the product is : " + product);
-        float sum = number1+number2;
-        System.out.println("The sum is : " + sum);
-        float subtract = product-sum;
-        System.out.println("the result is " + subtract);
+        System.out.println("Enter the number : ");
+        int number = inputData.nextInt();
+        int product = 1;
+        int sum = 0;
+
+        for (int i = number; i>0 ; i/=10) {
+            int digit = i%10;
+            product=product*digit;
+            sum= sum+digit;
+        }
+        System.out.println("The product is : " + product);
+        System.out.println("The sum is  : " + sum);
+        int subtract = product-sum;
+        System.out.println("The result is : " + subtract);
     }
 }
